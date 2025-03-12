747fc304bab41cb2c1b7970dda38b96c7ca19fe0
Mauricio Torti <mauriciotorti1@gmail.com>
2025-03-12 11:08:43 -0300

    Agregado de pipes poco comunes

--- src/app/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,9 +1,93 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { CardComponent } from '../../components/card/card.component';
+import { AsyncPipe, I18nPluralPipe, I18nSelectPipe, JsonPipe, KeyValuePipe, SlicePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { interval, map, tap } from 'rxjs';
+
+const client1 = {
+  name: 'Fernando',
+  gender: 'male',
+  age: 39,
+  address: 'Ottawa, Canadá',
+};
+
+const client2 = {
+  name: 'Melissa',
+  gender: 'female',
+  age: 33,
+  address: 'Toronto, Canadá',
+};
+
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [],
+  imports: [CardComponent, I18nSelectPipe, I18nPluralPipe, SlicePipe, JsonPipe, UpperCasePipe, KeyValuePipe, AsyncPipe, TitleCasePipe],
   templateUrl: './uncommon-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class UncommonPageComponent { }
+export default class UncommonPageComponent {
+
+
+
+  // i18nSelectPipe
+  client = signal(client1)
+
+  changeClient() {
+    if (this.client() === client1) {
+      this.client.set(client2);
+      return;
+    }
+
+    this.client.set(client1);
+  }
+
+  invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
+
+  // i18nPluralPipe
+
+  clientsMap = signal({
+    '=0': 'no tenemos ningún cliente esperando',
+    '=1': 'tenemos un cliente esperando',
+    '=2': 'tenemos 2 clientes esperando',
+    other: 'tenemos # clientes esperando',
+  });
+
+  clients = signal([
+    'Maria',
+    'Pedro',
+    'Fernando',
+    'Melissa',
+    'Natalia',
+    'Andrea',
+    'Juan',
+    'Carlos',
+  ]);
+
+  deleteClient() {
+    this.clients.update((prev) => prev.slice(1) )
+    }
+
+    profile = {
+      name: 'Fernando',
+      age: 39,
+      address: 'Ottawa, Canada',
+    };
+
+    // Async Pipe
+    promiseValue: Promise<string> = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        // reject('Tenemos un error en la data');
+        resolve('Tenemos data en la promesa.');
+        console.log('Promesa finalizada');
+      }, 3500);
+    });
+
+    myObservableTimer = interval(2000).pipe(
+      map((value) => value + 1),
+      tap((value) => console.log('tap:', value))
+    );
+
+
+}
